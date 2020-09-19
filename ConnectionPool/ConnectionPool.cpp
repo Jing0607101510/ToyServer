fd4f8325ad944a9a2ae4e0d6b72fc5bb5359ebd9
Jing0607101510 <1293521172@qq.com>
2020-09-19 13:35:33 +0800

    动态连接池1

--- ConnectionPool/ConnectionPool.cpp
+++ ConnectionPool/ConnectionPool.cpp
@@ -15,3 +15,35 @@ ConnectionPool& ConnectionPool::getInstance(){
     }
     return *pool;
 }
+
+
+// 默认配置
+ConnectionPool::ConnectionPool(){
+    max_num_conn = 20;
+    min_num_conn = 2;
+    cur_num_conn = 0;
+    busy_num_conn = 0;
+
+    user = "user";
+    passwd = "user";
+    url = "127.0.0.1";
+    db_name = "users";
+    port = 3306;
+}
+
+
+void  ConnectionPool::init(std::string url, int port, std::string db_name, std::string user, std::string passwd, 
+                  int min_num_conn, int max_num_conn){
+
+
+}
+
+void ConnectionPool::manage_connections(); // 管理线程
+void ConnectionPool::destroy_pool(); // 销毁连接池
+void ConnectionPool::expand_pool(int size); // 扩大连接池
+void ConnectionPool::reduce_pool(int size); // 缩小连接池
+ConnectionPool::~ConnectionPool();
+
+MySQL* ConnectionPool::getConnection();
+        // 归还连接
+void ConnectionPool::retConnection(MySQL* conn);
--- ConnectionPool/ConnectionPool.h
+++ ConnectionPool/ConnectionPool.h
@@ -10,17 +10,26 @@
 #include <mutex>
 #include <list>
 #include <thread>
-
+#include <memory>
+#include <string>
 
 class ConnectionPool{
     public:
         static ConnectionPool& getInstance();
+        void init(std::string url, int port, std::string db_name, std::string user, std::string passwd, 
+                  int min_num_conn, int max_num_conn);
+        // 获取连接
         MySQL* getConnection();
+        // 归还连接
         void retConnection(MySQL* conn);
 
     private:
-        
-    public:
+        void manage_connections(); // 管理线程
+        void destroy_pool(); // 销毁连接池
+        void expand_pool(int size); // 扩大连接池
+        void reduce_pool(int size); // 缩小连接池
+        ~ConnectionPool();
+        ConnectionPool();
 
     private:
         // 单例模式
@@ -29,13 +38,34 @@ class ConnectionPool{
 
         // 连接池，使用list实现
         std::list<MySQL*> conns;
+        std::mutex conn_mtx; // 连接池链表的互斥
 
-        // 管理线程
-        std::thread manager
+        // 管理线程 用于定时扩缩连接池
+        std::shared_ptr<std::thread> sp_thread;
+
+        // 连接池容量相关
+        int max_num_conn; // 最大连接数
+        int min_num_conn; // 最小连接数
+        int cur_num_conn; // 当前建立的连接数
+        int busy_num_conn; // 正在被使用的连接数
+
+        // 数据库连接相关
+        std::string user;
+        std::string passwd;
+        std::string url;
+        std::string db_name;
+        int db_port;
+
+        
 };
 
 class ConnectionRAII{
-
+    public:
+        ConnectionRAII(MySQL** conn, ConnectionPool* pool);
+        ~COnnectionRAII(); // 析构时归还
+    private:
+        MySQL* conn;
+        ConnectionPool* pool;
 };
 
 #endif
